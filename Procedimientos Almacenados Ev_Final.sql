8cc78e240473b5ebf9c8796d99028c0c55b83411
jfurlano <61610110+jfurlano@users.noreply.github.com>
2020-11-12 20:27:30 -0600

    Create Procedimientos Almacenados Ev_Final.sql
    Creación del SQLQuery que contiene los procedimientos almacenados utilizados en el proyecto.

---
+++ Procedimientos Almacenados Ev_Final.sql
@@ -0,0 +1,6 @@
+----------------------- SQLQUERY EVALUACION FINAL--------------------------
+CREATE SCHEMA Sistema
+
+ALTER SCHEMA Sistema TRANSFER dbo.TblContinentes
+ALTER SCHEMA Sistema TRANSFER dbo.TblPaises
+
